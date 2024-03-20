b4fa95a39f3abc97c7647870e0fe399391ebd0a0
skuqre <74248935+skuqre@users.noreply.github.com>
2024-03-20 21:24:37 +0800

    feat: reoccuring event banners
    uncomment to see
    specifically proud of the full burst day banner you love to see it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://skuqre.github.io',
-    base: '/anisa',
-    vite: {
-        server: {
-            fs: {
-                strict: false
-            }
-        }
+  site: 'https://skuqre.github.io',
+  base: '/anisa',
+  vite: {
+    server: {
+      fs: {
+        strict: false
+      }
     }
+  },
+  integrations: [mdx()]
 });
